bf229ee93d21e76d552b0b604b899f283b5a3eb7
Leon-4242 <leon.zazovskii@math.msu.ru>
2024-10-15 16:19:29 +0300

    upd

--- vika/test.cpp
+++ vika/test.cpp
@@ -1,5 +1,25 @@
-#include "unique_ptr_vector.h"
+#include <iostream>
 
-int main(void) {
+bool func(int);
+
+int main(){
+	int x = 32;
+	std::cout << func(x) << std::endl; 
+
 	return 0;
+}
+
+bool func(int x){
+/*	if (x == 1){
+		return(true);
+	}
+	if (x%2 == 1){
+		return(false);
+	}
+*/
+	if (x%2 == 1) {
+		return (x == 1) ? true : false;
+	}
+
+	return func(x/2);
 }
