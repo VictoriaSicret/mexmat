3f8c75e60f35b62a135e3f861e1e098fb5db0060
Leon-4242 <leon.zazovskii@math.msu.ru>
2024-10-15 18:09:21 +0300

    release

---
+++ third task/interface.h
@@ -0,0 +1,12 @@
+#ifndef InterFace3
+#define InterFace3
+
+#include "3.h"
+
+void func(void);
+bool input(int &action, bool &data1, bool &data2);
+
+bool choice(int tmp, bool &flag);
+cint::CintN make(bool flag_d, bool &flag);
+
+#endif
