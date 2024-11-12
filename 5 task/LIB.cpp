c6c775475f8838c7dd9accb8889805e9669db1cc
Leon-4242 <leon.zazovskii@math.msu.ru>
2024-11-12 15:30:04 +0300

    rebuild

--- 5 task/LIB.cpp
+++ 5 task/LIB.cpp
@@ -10,3 +10,20 @@ namespace EXCEPT {
 	Except::~Except() {	}
     
 }
+
+namespace CMP {
+	int cmp (const std::string str1, const std::string str2) {
+        if (cmplen(str1, str2) != 0) return cmplen(str1, str2);
+        if (str1 > str2) return 1;
+        if (str1 < str2) return -1;
+
+        return 0;
+    }
+    
+    int cmplen (const std::string str1, const std::string str2) {
+        if (str1.length() < str2.length()) return -1;
+        if (str1.length() > str2.length()) return 1;
+        return 0;
+    }
+    
+}
