dc8b4a9910f9bff8cc9539e921dd2c80cf7c8425
Leon-4242 <leon.zazovskii@math.msu.ru>
2024-11-25 20:16:59 +0300

    Update test.cpp

--- 6 task/test.cpp
+++ 6 task/test.cpp
@@ -3,8 +3,8 @@
 using namespace RBTREE;
 int main(void) {
     RBTree<int, int> tree; int x = 0;
-    tree.insert(2, 5); tree.insert(1, 5);
+    tree.insert(1, 5); tree.insert(2, 10), tree.insert(3, 10)
     if(tree.find(2, x)) std::cout << tree[2] << " " <<x << std::endl;
-    tree.remove(1);
+    tree.remove(2);
     return 0;
 }
